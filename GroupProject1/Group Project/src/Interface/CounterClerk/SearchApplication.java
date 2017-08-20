10265cc0ff57b95c50c80ddc5a05d1adb5a1b0fd
amiladarshana1994 <31099677+amiladarshana1994@users.noreply.github.com>
2017-08-20 15:06:33 +0530

    interface

--- GroupProject1/Group Project/src/Interface/CounterClerk/SearchApplication.java
+++ GroupProject1/Group Project/src/Interface/CounterClerk/SearchApplication.java
@@ -32,6 +32,7 @@ public class SearchApplication extends javax.swing.JFrame {
         SearchDetails_SearchTextFeild = new javax.swing.JTextField();
         SearchApplicant_Combo = new javax.swing.JComboBox();
         SearchApplication_Combo = new javax.swing.JComboBox();
+        CounterClerk_Search = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
@@ -54,20 +55,27 @@ public class SearchApplication extends javax.swing.JFrame {
             }
         });
 
-        SearchApplicant_Combo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Search Applicant", "NIC", "Name", "Last Name" }));
+        SearchApplicant_Combo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Application ID", "Owner NIC", "Name", "Last Name" }));
 
-        SearchApplication_Combo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Search Application", "Application ID", "Owner NIC" }));
+        SearchApplication_Combo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Search Application", "Search Applicant" }));
+
+        CounterClerk_Search.setText("Search");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(SearchDetails_SearchTextFeild, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(SearchApplication_Combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(SearchApplicant_Combo, 0, 133, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(SearchDetails_SearchTextFeild, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(26, 26, 26)
+                        .addComponent(SearchApplication_Combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(SearchApplicant_Combo, 0, 133, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(CounterClerk_Search)))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -78,7 +86,8 @@ public class SearchApplication extends javax.swing.JFrame {
                     .addComponent(SearchDetails_SearchTextFeild, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(SearchApplicant_Combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(SearchApplication_Combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(44, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
+                .addComponent(CounterClerk_Search))
         );
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, 460, 110));
@@ -87,13 +96,13 @@ public class SearchApplication extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Title 1", "Title 2"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -176,6 +185,7 @@ public class SearchApplication extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton CounterClerk_Search;
     private javax.swing.JComboBox SearchApplicant_Combo;
     private javax.swing.JComboBox SearchApplication_Combo;
     private javax.swing.JTextField SearchDetails_SearchTextFeild;
